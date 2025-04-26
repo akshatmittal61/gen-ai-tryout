c5edf2686654ba6b8d46fb0182d676ecd0aec3e0
Akshat Mittal <akshatmittal2506@gmail.com>
2025-04-26 20:48:28 +0530

    fix: minor cleanup

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 import os
 from config import groq_key
-# from langchain_community.llms import HuggingFacePipeline
 from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
-
-# os.environ['OPENAI_API_KEY'] = openapi_key
-#
-# llm = HuggingFacePipeline.from_model_id(
-#     model_id="gpt2",
-#     task="text-generation",
-#     pipeline_kwargs={"max_new_tokens": 10},
-#     device=-1
-# )
 
 os.environ['GROQ_API_KEY'] = groq_key
 
@@ -28,14 +18,6 @@ Answer: Let's think step by step."""
 
 prompt = PromptTemplate.from_template(template)
 
-# questions = [
-#     "What is current population of the world",
-#     "What are symlinks"
-# ]
-
 question = "What are symlinks"
 chain = prompt | llm
 print(chain.invoke({"question": question}))
-
-# for question in questions:
-#     print(chain.invoke({"question": question}))
